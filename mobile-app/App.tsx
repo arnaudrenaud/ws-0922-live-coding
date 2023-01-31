5bbe759d2575580da9659e854f44210ac64da29f
Arnaud Renaud <arnaud.renaud@gmail.com>
2023-01-31 10:55:21 +0100

    Show Expo push token, display notifications
    Live coding : commencer déconnecté d'Expo CLI, ce qui fera un message d'erreur, puis se connecter : `npx expo login`.

--- mobile-app/App.tsx
+++ mobile-app/App.tsx
@@ -6,6 +6,8 @@ import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import useCachedResources from "./hooks/useCachedResources";
 import useColorScheme from "./hooks/useColorScheme";
 import Navigation from "./navigation";
+import { View, Text } from "react-native";
+import { usePushNotifications } from "./push-notifications";
 
 const GRAPHQL_API_URL = Constants?.expoConfig?.extra?.GRAPHQL_API_URL;
 
@@ -18,12 +20,36 @@ export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
 
+  const { expoPushToken, notification } = usePushNotifications();
+
   if (!isLoadingComplete) {
     return null;
   } else {
     return (
       <SafeAreaProvider>
         <ApolloProvider client={client}>
+          <View
+            style={{
+              flex: 1,
+              alignItems: "center",
+              justifyContent: "space-around",
+            }}
+          >
+            <Text>Your expo push token: {expoPushToken}</Text>
+            <View style={{ alignItems: "center", justifyContent: "center" }}>
+              <Text>
+                Title: {notification && notification.request.content.title}{" "}
+              </Text>
+              <Text>
+                Body: {notification && notification.request.content.body}
+              </Text>
+              <Text>
+                Data:{" "}
+                {notification &&
+                  JSON.stringify(notification.request.content.data)}
+              </Text>
+            </View>
+          </View>
           <Navigation colorScheme={colorScheme} />
           <StatusBar />
         </ApolloProvider>
---
+++ mobile-app/push-notifications.tsx
@@ -0,0 +1,82 @@
+import * as Device from "expo-device";
+import * as Notifications from "expo-notifications";
+import { Subscription } from "expo-modules-core";
+import { useState, useRef, useEffect } from "react";
+import { Platform } from "react-native";
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
+export async function registerForPushNotificationsAsync() {
+  let token;
+  if (Device.isDevice) {
+    const { status: existingStatus } =
+      await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+    if (existingStatus !== "granted") {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
+    }
+    if (finalStatus !== "granted") {
+      alert("Failed to get push token for push notification!");
+      return;
+    }
+    token = (await Notifications.getExpoPushTokenAsync()).data;
+    console.log(token);
+  } else {
+    alert("Must use physical device for Push Notifications");
+  }
+
+  if (Platform.OS === "android") {
+    Notifications.setNotificationChannelAsync("default", {
+      name: "default",
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: "#FF231F7C",
+    });
+  }
+
+  return token;
+}
+
+export const usePushNotifications = () => {
+  const [expoPushToken, setExpoPushToken] = useState<string | undefined>("");
+  const [notification, setNotification] = useState<
+    Notifications.Notification | false
+  >(false);
+  const notificationListener = useRef<Subscription>();
+  const responseListener = useRef<Subscription>();
+
+  useEffect(() => {
+    registerForPushNotificationsAsync().then((token) =>
+      setExpoPushToken(token)
+    );
+
+    notificationListener.current =
+      Notifications.addNotificationReceivedListener((notification) => {
+        setNotification(notification);
+      });
+
+    responseListener.current =
+      Notifications.addNotificationResponseReceivedListener((response) => {
+        console.log(response);
+      });
+
+    return () => {
+      if (notificationListener.current) {
+        Notifications.removeNotificationSubscription(
+          notificationListener.current
+        );
+      }
+      if (responseListener.current) {
+        Notifications.removeNotificationSubscription(responseListener.current);
+      }
+    };
+  }, []);
+
+  return { expoPushToken, notification };
+};
