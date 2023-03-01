381ef041bb46be16d7ccb6c4e132a7fbe99f33a8
Arnaud Renaud <arnaud.renaud@gmail.com>
2023-03-01 16:10:38 +0100

    Improve formatting auto commit message

--- web-app/src/index.tsx
+++ web-app/src/index.tsx
@@ -7,6 +7,12 @@ import "./index.css";
 import App from "./App/App";
 import reportWebVitals from "./reportWebVitals";
 
+
+
+
+
+
+
 const client = new ApolloClient({
   uri: "/",
   cache: new InMemoryCache({
