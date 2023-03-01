fc577de9a7ca9dd26a836ba83ead34b15a9d0823
Arnaud Renaud <arnaud.renaud@gmail.com>
2023-03-01 11:33:37 +0100

    Add command to fix formatting on whole project (TS, TSX files)

--- mobile-app/gql/graphql.ts
+++ mobile-app/gql/graphql.ts
@@ -1,10 +1,16 @@
 /* eslint-disable */
-import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/core';
+import { TypedDocumentNode as DocumentNode } from "@graphql-typed-document-node/core";
 export type Maybe<T> = T | null;
 export type InputMaybe<T> = Maybe<T>;
-export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
-export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
-export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+export type Exact<T extends { [key: string]: unknown }> = {
+  [K in keyof T]: T[K];
+};
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & {
+  [SubKey in K]?: Maybe<T[SubKey]>;
+};
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & {
+  [SubKey in K]: Maybe<T[SubKey]>;
+};
 /** All built-in and custom scalars, mapped to their actual values */
 export type Scalars = {
   ID: string;
@@ -15,15 +21,15 @@ export type Scalars = {
 };
 
 export type AppUser = {
-  __typename?: 'AppUser';
-  emailAddress: Scalars['String'];
-  firstName: Scalars['String'];
-  id: Scalars['ID'];
-  lastName: Scalars['String'];
+  __typename?: "AppUser";
+  emailAddress: Scalars["String"];
+  firstName: Scalars["String"];
+  id: Scalars["ID"];
+  lastName: Scalars["String"];
 };
 
 export type Mutation = {
-  __typename?: 'Mutation';
+  __typename?: "Mutation";
   addSkillToWilder: Wilder;
   approveWilder: Wilder;
   createWilder: Wilder;
@@ -33,85 +39,112 @@ export type Mutation = {
   updateWilder: Wilder;
 };
 
-
 export type MutationAddSkillToWilderArgs = {
-  skillId: Scalars['String'];
-  wilderId: Scalars['String'];
+  skillId: Scalars["String"];
+  wilderId: Scalars["String"];
 };
-
 
 export type MutationApproveWilderArgs = {
-  id: Scalars['String'];
+  id: Scalars["String"];
 };
-
 
 export type MutationCreateWilderArgs = {
-  firstName: Scalars['String'];
-  lastName: Scalars['String'];
+  firstName: Scalars["String"];
+  lastName: Scalars["String"];
 };
-
 
 export type MutationDeleteWilderArgs = {
-  id: Scalars['String'];
+  id: Scalars["String"];
 };
-
 
 export type MutationSignInArgs = {
-  emailAddress: Scalars['String'];
-  password: Scalars['String'];
+  emailAddress: Scalars["String"];
+  password: Scalars["String"];
 };
-
 
 export type MutationSignUpArgs = {
-  emailAddress: Scalars['String'];
-  firstName: Scalars['String'];
-  lastName: Scalars['String'];
-  password: Scalars['String'];
+  emailAddress: Scalars["String"];
+  firstName: Scalars["String"];
+  lastName: Scalars["String"];
+  password: Scalars["String"];
 };
 
-
 export type MutationUpdateWilderArgs = {
-  firstName: Scalars['String'];
-  id: Scalars['ID'];
-  lastName: Scalars['String'];
+  firstName: Scalars["String"];
+  id: Scalars["ID"];
+  lastName: Scalars["String"];
 };
 
 export type Query = {
-  __typename?: 'Query';
+  __typename?: "Query";
   myProfile: AppUser;
   wilders: Array<Wilder>;
 };
 
 export type School = {
-  __typename?: 'School';
-  id: Scalars['ID'];
-  schoolName: Scalars['String'];
+  __typename?: "School";
+  id: Scalars["ID"];
+  schoolName: Scalars["String"];
   wilders: Array<Wilder>;
 };
 
 export type Skill = {
-  __typename?: 'Skill';
-  id: Scalars['ID'];
-  skillName: Scalars['String'];
+  __typename?: "Skill";
+  id: Scalars["ID"];
+  skillName: Scalars["String"];
   wilders: Array<Wilder>;
 };
 
 export type Wilder = {
-  __typename?: 'Wilder';
-  firstName: Scalars['String'];
-  getDisplayName: Scalars['String'];
-  getFullName: Scalars['String'];
-  id: Scalars['ID'];
-  isApproved: Scalars['Boolean'];
-  lastName: Scalars['String'];
+  __typename?: "Wilder";
+  firstName: Scalars["String"];
+  getDisplayName: Scalars["String"];
+  getFullName: Scalars["String"];
+  id: Scalars["ID"];
+  isApproved: Scalars["Boolean"];
+  lastName: Scalars["String"];
   school?: Maybe<School>;
   skills: Array<Skill>;
 };
 
-export type GetWildersQueryVariables = Exact<{ [key: string]: never; }>;
+export type GetWildersQueryVariables = Exact<{ [key: string]: never }>;
 
+export type GetWildersQuery = {
+  __typename?: "Query";
+  wilders: Array<{
+    __typename?: "Wilder";
+    id: string;
+    firstName: string;
+    lastName: string;
+    isApproved: boolean;
+  }>;
+};
 
-export type GetWildersQuery = { __typename?: 'Query', wilders: Array<{ __typename?: 'Wilder', id: string, firstName: string, lastName: string, isApproved: boolean }> };
-
-
-export const GetWildersDocument = {"kind":"Document","definitions":[{"kind":"OperationDefinition","operation":"query","name":{"kind":"Name","value":"GetWilders"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"wilders"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"id"}},{"kind":"Field","name":{"kind":"Name","value":"firstName"}},{"kind":"Field","name":{"kind":"Name","value":"lastName"}},{"kind":"Field","name":{"kind":"Name","value":"isApproved"}}]}}]}}]} as unknown as DocumentNode<GetWildersQuery, GetWildersQueryVariables>;
+export const GetWildersDocument = {
+  kind: "Document",
+  definitions: [
+    {
+      kind: "OperationDefinition",
+      operation: "query",
+      name: { kind: "Name", value: "GetWilders" },
+      selectionSet: {
+        kind: "SelectionSet",
+        selections: [
+          {
+            kind: "Field",
+            name: { kind: "Name", value: "wilders" },
+            selectionSet: {
+              kind: "SelectionSet",
+              selections: [
+                { kind: "Field", name: { kind: "Name", value: "id" } },
+                { kind: "Field", name: { kind: "Name", value: "firstName" } },
+                { kind: "Field", name: { kind: "Name", value: "lastName" } },
+                { kind: "Field", name: { kind: "Name", value: "isApproved" } },
+              ],
+            },
+          },
+        ],
+      },
+    },
+  ],
+} as unknown as DocumentNode<GetWildersQuery, GetWildersQueryVariables>;
